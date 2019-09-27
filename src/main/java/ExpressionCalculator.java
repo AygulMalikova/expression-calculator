ca4ce7a4192855bacee32d6e05bff2ba92bac4e5
aygulmalikova <amalikova19@gmail.com>
2019-09-27 20:30:30 +0300

    + Adding comments

--- src/main/java/ExpressionCalculator.java
+++ src/main/java/ExpressionCalculator.java
@@ -5,11 +5,8 @@ import java.io.InputStream;
 
 
 public class ExpressionCalculator {
-    private static final char EOF = (char) -1;
-
     public static void main(String[] args) throws IOException {
-        InputStream file = ExpressionCalculator.class.getClassLoader().getResourceAsStream("test4.txt");
-
+        InputStream file = ExpressionCalculator.class.getClassLoader().getResourceAsStream("test1.txt");
         Parser parser = new Parser(file);
         Expression parsedExpr = parser.parse();
         if (parsedExpr == null) {
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -21,29 +21,34 @@ public class Parser {
     public Parser(InputStream input) {
         this.input = new InputStreamReader(input, Charset.forName("UTF-8"));
     }
+
+    //starting point of the program
     public Expression parse() throws IOException {
-        Character op;
-        if ((op = readSymbol()) != null) {
-            this.current = op;
+        Character character;
+        if ((character = readSymbol()) != null) {
+            this.current = character; //setting first char as current
         }
         else {
             return null;
         }
         return parseRelation();
     }
+
     private Expression parseRelation() throws IOException {
         Expression result = parseTerm();
         if (result == null) {
             return null;
         }
+        //reading relation characters until stopping criteria (or until end of the file)
         while ( !endOdFile ) {
             Character temp = current;
             if (current == '<' || current == '>' || current == '=') {
-                Character op;
-                if ((op = readSymbol()) == null) {
+                Character character;
+                if ((character = readSymbol()) == null) {
                     return result;
                 }
-                current = op;
+                current = character;
+                //reading the right part of expression if it exists
                 Expression right = parseTerm();
                 if (right == null) {
                     return result;
@@ -66,14 +71,15 @@ public class Parser {
         if (result == null) {
             return null;
         }
+        //reading terms (+/-) until stopping criteria (or until end of the file)
         while ( !endOdFile ) {
             Character temp = current;
             if (current == '-' || current == '+') {
-                Character op;
-                if ((op = readSymbol()) == null) {
+                Character character;
+                if ((character = readSymbol()) == null) {
                     return result;
                 }
-                current = op;
+                current = character;
                 Expression right = parseFactor();
                 if (right == null) {
                     return result;
@@ -94,14 +100,14 @@ public class Parser {
         if (result == null) {
             return null;
         }
-
+        //reading factor (*) until stopping criteria (or until end of the file)
         while ( !endOdFile ) {
             if (current == '*') {
-                Character op;
-                if ((op = readSymbol()) == null) {
+                Character character;
+                if ((character = readSymbol()) == null) {
                     return result;
                 }
-                current = op;
+                current = character;
                 Expression right = parsePrimary();
                 if (right == null) {
                     return result;
@@ -117,6 +123,7 @@ public class Parser {
     private Expression parsePrimary() throws IOException {
         Expression result = null;
 
+        // firstly, we need to check whether current char is digit or opening parenthesis
         if (Character.isDigit(current) ) {
             result = parseInteger(Character.getNumericValue(current));
         }
@@ -131,31 +138,35 @@ public class Parser {
     private Expression parseInteger(int firstDigit) throws IOException {
         Expression result = null;
         String number = Integer.toString(firstDigit);
-        Character op;
-        while ((op = readSymbol()) != null && Character.isDigit(op)) {
-            number = number.concat(String.valueOf(op));
+        Character character;
+        //reading the char until they are digits and build the string
+        while ((character = readSymbol()) != null && Character.isDigit(character)) {
+            number = number.concat(String.valueOf(character));
         }
-        if (op != null)
-            current = op;
+        //if there is more characters then assign new one to current
+        if (character != null)
+            current = character;
 
         return new Int(Integer.parseInt(number));
 
     }
     private void skipNextChar() throws IOException {
         Character op;
+        //skipping the closing parenthesis
         while ((op = readSymbol()) != null && op == ')') {
             current = op;
         }
         if (op != null)
             current = op;
     }
+
     private Character readSymbol() throws IOException {
+        //reading the stream of chars from input file
         char nextChar = (char) input.read();
         if (nextChar == EOF) {
             endOdFile = true;
             return null;
         }
-            return nextChar;
-
+        return nextChar;
     }
 }
